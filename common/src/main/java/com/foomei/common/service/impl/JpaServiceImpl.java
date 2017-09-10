cf969f0fd1fcbf3a514aee122ed6e87211894852
wumin <84760025@qq.com>
2017-09-10 22:52:31 +0800

    增加测试

--- common/src/main/java/com/foomei/common/service/impl/JpaServiceImpl.java
+++ common/src/main/java/com/foomei/common/service/impl/JpaServiceImpl.java
@@ -20,70 +20,70 @@ import com.foomei.common.service.JpaService;
 
 public abstract class JpaServiceImpl<DAO extends JpaDao<T, ID>, T, ID extends Serializable> implements JpaService<DAO, T, ID> {
 
-    protected static final Logger logger = LoggerFactory.getLogger(JpaServiceImpl.class);
+  protected static final Logger logger = LoggerFactory.getLogger(JpaServiceImpl.class);
 
-	protected DAO dao;
-    protected Class<T> entityClazz;
+  protected DAO dao;
+  protected Class<T> entityClazz;
 
-	public JpaServiceImpl() {
-        this.entityClazz = ClassUtil.getClassGenricType(getClass(), 1);
-	}
+  public JpaServiceImpl() {
+    this.entityClazz = ClassUtil.getClassGenricType(getClass(), 1);
+  }
 
-	public T get(ID id) {
-		return dao.findOne(id);
-	}
+  public T get(ID id) {
+    return dao.findOne(id);
+  }
 
-	public List<T> getAll() {
-		return dao.findAll();
-	}
+  public List<T> getAll() {
+    return dao.findAll();
+  }
 
-	public T save(T entity) {
-		T t = dao.save(entity);
-		logger.info("save entity: {}", entity);
-		return t;
-	}
+  public T save(T entity) {
+    T t = dao.save(entity);
+    logger.info("save entity: {}", entity);
+    return t;
+  }
 
-	public void delete(ID id) {
-		dao.delete(id);
-		logger.info("delete entity {}, id is {}", entityClazz.getName(), id);
-	}
-	
-	public List<T> getList(SearchFilter searchFilter, Sort sort) {
-		Specification<T> spec = DynamicSpecification.bySearchFilter(searchFilter, entityClazz);
-		return dao.findAll(spec, sort);
-	}
-	
-	public List<T> getList(Map<String, Object> searchParams) {
-	    SearchFilter searchFilter = SearchFilter.parse(searchParams);
-		return getList(searchFilter, null);
-	}
-	
-	public Page<T> getPage(SearchFilter searchFilter, Pageable page) {
-		Specification<T> spec = DynamicSpecification.bySearchFilter(searchFilter, entityClazz);
-		return dao.findAll(spec, page);
-	}
+  public void delete(ID id) {
+    dao.delete(id);
+    logger.info("delete entity {}, id is {}", entityClazz.getName(), id);
+  }
 
-	public Page<T> getPage(Map<String, Object> searchParams, Pageable page) {
-		SearchFilter searchFilter = SearchFilter.parse(searchParams);
-		return getPage(searchFilter, page);
-	}
+  public List<T> getList(SearchFilter searchFilter, Sort sort) {
+    Specification<T> spec = DynamicSpecification.bySearchFilter(searchFilter, entityClazz);
+    return dao.findAll(spec, sort);
+  }
 
-	public Page<T> getPage(JqGridFilter jqGridFilter, Pageable page) {
-	    SearchFilter searchFilter = SearchFilter.parse(jqGridFilter);
-		return getPage(searchFilter, page);
-	}
-	
-	public Long count(SearchFilter searchFilter) {
-	    Specification<T> spec = DynamicSpecification.bySearchFilter(searchFilter, entityClazz);
-        return dao.count(spec);
-    }
-	
-	public Long count(Map<String, Object> searchParams) {
-	    SearchFilter searchFilter = SearchFilter.parse(searchParams);
-	    return count(searchFilter);
-	}
+  public List<T> getList(Map<String, Object> searchParams) {
+    SearchFilter searchFilter = SearchFilter.parse(searchParams);
+    return getList(searchFilter, null);
+  }
 
-	public void setDao(DAO dao) {
-        this.dao = dao;
-    }
+  public Page<T> getPage(SearchFilter searchFilter, Pageable page) {
+    Specification<T> spec = DynamicSpecification.bySearchFilter(searchFilter, entityClazz);
+    return dao.findAll(spec, page);
+  }
+
+  public Page<T> getPage(Map<String, Object> searchParams, Pageable page) {
+    SearchFilter searchFilter = SearchFilter.parse(searchParams);
+    return getPage(searchFilter, page);
+  }
+
+  public Page<T> getPage(JqGridFilter jqGridFilter, Pageable page) {
+    SearchFilter searchFilter = SearchFilter.parse(jqGridFilter);
+    return getPage(searchFilter, page);
+  }
+
+  public Long count(SearchFilter searchFilter) {
+    Specification<T> spec = DynamicSpecification.bySearchFilter(searchFilter, entityClazz);
+    return dao.count(spec);
+  }
+
+  public Long count(Map<String, Object> searchParams) {
+    SearchFilter searchFilter = SearchFilter.parse(searchParams);
+    return count(searchFilter);
+  }
+
+  public void setDao(DAO dao) {
+    this.dao = dao;
+  }
 }
